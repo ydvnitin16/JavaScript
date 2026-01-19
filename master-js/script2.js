c1f2142e364e7ae9f2791040920d5d905320a1cd
Nitin Yadav <ydvnitin16@gmail.com>
2026-01-19 11:45:54 +0530

    implemented memoize fn and corrected a bugged code

---
+++ master-js/script2.js
@@ -0,0 +1,51 @@
+//  Create a counter
+
+// function createCounter() {
+//     let count = 0;
+
+//     return () => {
+//         return ++count;
+//     };
+// }
+
+// Memoize function
+
+// function memoize(fn) {
+//     const cache = new Map();
+
+//     return (...args) => {
+//         const key = JSON.stringify(args);
+//         if (cache.has(key)) {
+//             return cache.get(key);
+//         }
+//         console.log('Executing fn for: ', key);
+//         const result = fn(...args);
+//         cache.set(key, result);
+//         return result;
+//     };
+// }
+
+// function add(a, b) {
+//     return a + b;
+// }
+
+// const memoizedAdd = memoize(add);
+
+// memoizedAdd(2, 3);
+// memoizedAdd(2, 3);
+// memoizedAdd(4, 5);
+// memoizedAdd(2, 3);
+
+
+// solve the buggy code given below:
+
+// var i = 0; 1-2-3-4 console.log('var value'); global scope
+// let-const i = 0; 1-2-3-4 console.log('it prints the value at the time of iteration') block scope
+
+// for (var i = 0; i < 5; i++) {
+//     setTimeout(() => {
+//         console.log(i);
+//     }, 1000);
+// }
+
+// solution: change the variable type var -> let
