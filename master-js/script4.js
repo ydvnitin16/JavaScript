00a3344183c993d61516e0c4df353a30bc7ec8d7
Nitin Yadav <ydvnitin16@gmail.com>
2026-04-27 11:37:35 +0530

    Practiced: Implement a memoize() utility function

--- master-js/script4.js
+++ master-js/script4.js
@@ -20,3 +20,28 @@ function longestSubStrWithoutRepeatingChar(str) {
 }
 
 console.log(longestSubStrWithoutRepeatingChar(str));
+
+// Implement a memoize() utility function
+
+function memoizeFn(fn) {
+    const cache = new Map();
+
+    return (...args) => {
+        const key = JSON.stringify(...args);
+        if (cache.has(key)) {
+            return cache.get(key);
+        }
+
+        const value = fn(...args);
+        cache.set(key, value);
+        return value;
+    };
+}
+
+const sum = (a, b) => a + b;
+
+const memoizedSum = memoizeFn(sum);
+
+memoizedSum(1, 2);
+memoizedSum(3, 5);
+memoizedSum(1, 2);
